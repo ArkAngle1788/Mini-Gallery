5b2c0cb3fe476926aa5e75413ae5926c90fd7e5c
ArkAngle1788 <Nathaniel782@gmail.com>
2022-08-27 15:14:04 -0700

    pep8
    pep 8 formatting update, added a new try/catch for data verification, and cleaned up a bad try/catch into a filter.

--- CommunityInfrastructure/custom_functions.py
+++ CommunityInfrastructure/custom_functions.py
@@ -1,16 +1,36 @@
+from django.http import Http404
 import django.dispatch
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import PaintingStudio
 
 # this is for creating the through link model
 new_studio_image = django.dispatch.Signal()
 
-def studio_official_send(self,image):
 
-    studio=PaintingStudio.objects.get(pk=self.kwargs['pk'])
+def studio_official_send(self, image):
+    """sends the signal to update_studio_images"""
 
     try:
-        image.paintingstudio.get(id=studio.id)
-    except:
+        studio = PaintingStudio.objects.get(pk=self.kwargs['pk'])
+    except ObjectDoesNotExist as error:
+        raise Http404(f'{error}') from error
+
+    # if the studio the image is being posted from is
+    # not listed we add it first
+    if not image.paintingstudio.filter(id=studio.id):
         image.paintingstudio.add(studio)
 
     new_studio_image.send(sender=self.__class__, image=image, studio=studio)
+
+
+def studio_admin_check(self):
+    """
+    permission check for admin level control over painting studio pages.
+    Returns True if user has valid permissions and false otherwise
+    """
+    # the studio you're uploading to is always part of the url
+    if self.request.user.is_staff or self.request.user == PaintingStudio.objects.get(
+            pk=self.kwargs['pk']).userprofile:
+        return True
+    return False
