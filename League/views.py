142f02d7873a80f86a5aaf9535a061e36c4a730a
ArkAngle1788 <Nathaniel782@gmail.com>
2025-12-12 18:20:18 -0700

    oops
    I mean the previous error message was very clear that you need to add strings. I repeat my previous sentiment: Why did this work in testing?

--- League/views.py
+++ League/views.py
@@ -446,7 +446,7 @@ class SeasonClose(LoginRequiredMixin, UserPassesTestMixin, View):
                     else:
                         vp=(vp*num_rounds)/(rounds_played-1)
                     
-                    new_score_string=""+score_list[0]+","+int(op)+","+int(vp)
+                    new_score_string=""+score_list[0]+","+str(int(op))+","+str(int(vp))
                     if match.player1==player:
                         match.player1_score=new_score_string
                     elif match.player2==player:
